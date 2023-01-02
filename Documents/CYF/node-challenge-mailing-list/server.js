70dfcba192d0b3b09c6f0407c76640673a4b4a01
Nagehan Uyumaz <uyumaznagehan@gmail.com>
2023-01-02 22:56:43 +0000

    express

---
+++ Documents/CYF/node-challenge-mailing-list/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const PORT = process.env.PORT || 3001;
+const lists = new Map();
+
+app.use(express.json());
+app.use(cors());
+
+// Get all existing lists
+app.get("/lists", (req, res) => {
+  const listsArray = Array.from(lists.keys());
+  res.json(listsArray);
+});
+
+app.listen(PORT, function () {
+  console.log("Your app is listening on port " + PORT);
+});
